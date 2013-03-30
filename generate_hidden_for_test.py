d2925dd77b2d1b2f512ff6e94cb4c29d0dc17235
sauravmaximus <saurav@iupr.com>
2013-03-30 22:11:58 +0100

    Updated python file for generating hidden rep for training

--- generate_hidden_for_test.py
+++ generate_hidden_for_test.py
@@ -12,10 +12,10 @@ from theano.tensor.shared_randomstreams  import RandomStreams
 
 
 def find_hidden_rep(x,weights , bias):
- '''A function that when passed the input image representation ( as a 1D vector ) will return the hidden value representation from the autoencoder weights ''' 
+    '''A function that when passed the input image representation ( as a 1D vector ) will return the hidden value representation from the autoencoder weights ''' 
  
     activation = numpy.dot(x,weights)+bias 
-    hidden = 1./(1. + numpy.exp(-act))
+    hidden = 1./(1. + numpy.exp(-activation))
     return hidden 
 
 
@@ -29,15 +29,19 @@ if __name__ == "__main__":
    f3 = h5py.File("bias_trainedDA_8004.h5")
    trained_bias = f3['bias'][:]
    f3.close() 
-   f4 = h5py.File("labels_256_gray.h5")
+   f4 = h5py.File("../labels_256_gray.h5")
    labels = f4['labels'][:]
    f4.close() 
-   f5 = h5py.File("images_256_grayscale.h5")
+   f5 = h5py.File("../images_256_grayscale.h5")
    images = f5['images'][:]
    f5.close()
    test_set_images = images[6000:]
    test_set_labels = labels[6000:] 
    hidden_rep=[]  
    for image in test_set_images:
-      hidden_rep.append(find_hidden_rep(image,trained_weights,trained_bias)
-   
+      hidden_rep.append(find_hidden_rep(image,trained_weights,trained_bias))
+   hidden_train = numpy.array(hidden_rep)
+   f = h5py.File("training_set_hidden_rep.h5")
+   f["training_set_hidden"] = hidden_train
+   f.close()
+   print "My work is done " 
